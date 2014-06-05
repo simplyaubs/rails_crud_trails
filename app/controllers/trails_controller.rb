f5cf0e6f305ddf46cf9561c20a021d6c8659170e
Aubrey Howell <simplyaubs@gmail.com>
2014-06-05 13:40:34 -0600

    User can update a trail.

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -14,6 +14,21 @@ class TrailsController < ApplicationController
     end
   end
 
+  def show
+    @trail = Trail.find(params[:id])
+  end
+
+  def edit
+    @trail = Trail.find(params[:id])
+  end
+
+  def update
+    @trail = Trail.find(params[:id])
+    @trail.update_attributes!(trail_params)
+
+    redirect_to trails_path(@trail)
+  end
+
   private
   def trail_params
     params.require(:trail).permit(:name, :difficulty)
--- spec/features/trails_spec.rb
+++ spec/features/trails_spec.rb
@@ -11,4 +11,24 @@ feature 'CRUD favorite trails' do
     expect(page).to have_content 'Sinatas'
     expect(page).to have_content 4
   end
+
+  scenario 'User can update a trail' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a Trail'
+    fill_in 'Name', with: 'Sinatas'
+    fill_in 'Difficulty', with: 4
+    click_on 'Add Trail'
+    expect(page).to have_content 'Sinatas'
+    expect(page).to have_content 4
+    click_on 'Sinatas'
+    click_on 'Edit'
+    fill_in 'Name', with: 'Chautauqua'
+    fill_in 'Difficulty', with: 3
+    click_on 'Update'
+    expect(page).to have_content 'Chautauqua'
+    expect(page).to have_content 3
+    expect(page).to_not have_content 'Sinatas'
+    expect(page).to_not have_content 4
+  end
 end
